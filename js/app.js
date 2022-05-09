1a4d5fc42fd6c2978d80866660912e1df33b1048
maxgarr <maxime.garron17@gmail.com>
2022-05-09 18:50:27 +0200

    add js swipper

--- js/app.js
+++ js/app.js
@@ -42,3 +42,22 @@ slidingBandeau.forEach((bandeau) =>{
     
     })
 })
+
+// script js swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
