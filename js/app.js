36c18692d338945789197b3a1b2be1c197dd993e
NicoDev84 <carliernicolas84@gmail.com>
2022-05-09 18:38:12 +0200

    carousel swiper

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@ const btnFermer = document.querySelector(".btnClose");
 
 document.querySelectorAll('.image-container img').forEach(image =>{
     image.addEventListener("click", () =>{
-        document.querySelector('.popup-image').style.display = 'block';
-        document.querySelector('.popup-image img').src = image.getAttribute('src')
+        document.querySelector('.popup-slider').style.display = 'block';
+        document.querySelector('.popup-slider img').src = image.getAttribute('src')
     })
 });
 
 btnFermer.addEventListener("click", () =>{
-    document.querySelector('.popup-image').style.display = 'none';
+    document.querySelector('.popup-slider').style.display = 'none';
 })
 
 // ************* Animation Bandeau *********************** //
@@ -42,6 +42,24 @@ slidingBandeau.forEach((bandeau) =>{
     })
 })
 
+// script js swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
 
 
 
